6be14b2a923eec8c0f5fcc6a3e84574fd72e7802
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-16 01:53:18 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,68 +1,43 @@
 #!/usr/bin/python3
-"""Write a script that reads stdin line by line and computes metrics:
-
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size> (if the format is not this one, the line
-must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning:
-Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size>
-(see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer,
-don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
-
-line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
-<status code>, <file size>]
-"""
-
-
 import sys
+import re
+import signal
 
-# store the count of all status codes in a dictionary
-status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0}
 
-total_size = 0
-count = 0  # keep count of the number lines counted
+def handle(sig, frame):
+    if sig == signal.SIGINT:
+        return True
+    return False
 
-try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
 
-        if len(line_list) > 4:
-            status_code = line_list[-2]
-            file_size = int(line_list[-1])
+regex = (
+    r'(\d{1,3}(?:\.\d{1,3}){3}) - \[(.*?)\] '
+    r'"GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)'
+)
+num = 0
+size = 0
+lists = []
+# a = 0
 
-            # check if the status code receive exists in the dictionary and
-            # increment its count
-            if status_code in status_codes_dict.keys():
-                status_codes_dict[status_code] += 1
-
-            # update total size
-            total_size += file_size
-
-            # update count of lines
-            count += 1
-
-        if count == 10:
-            count = 0  # reset count
-            print('File size: {}'.format(total_size))
-
-            # print out status code counts
-            for key, value in sorted(status_codes_dict.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
-
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(status_codes_dict.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+while True:
+    line = sys.stdin.readline()
+    if not line:
+        break
+    line = line.strip()
+    match = re.match(regex, line)
+    if not match:
+        continue
+    string = line.split()
+    size += int(string[-1])
+    lists.append(string[-2])
+    num += 1
+    signals = signal.signal(signal.SIGINT, handle)
+    if num == 10 or signals:
+        print("Filse size: {}".format(size))
+        codes = [200, 301, 400, 401, 403, 404, 405, 500]
+        for c in codes:
+            sortedList = sorted(lists)
+            counts = sortedList.count(str(c))
+            if counts:
+                print('{}: {}'.format(c, counts))
+            continue
