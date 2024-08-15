e813f2ec3d15453a5dc346ec60f215a626a59ee7
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-16 01:54:18 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,39 @@
 #!/usr/bin/python3
+"""
+Log parsing
+"""
+
 import sys
-import re
-import signal
 
+if __name__ == '__main__':
 
-def handle(sig, frame):
-    if sig == signal.SIGINT:
-        return True
-    return False
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
-regex = (
-    r'(\d{1,3}(?:\.\d{1,3}){3}) - \[(.*?)\] '
-    r'"GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)'
-)
-num = 0
-size = 0
-lists = []
-# a = 0
-
-while True:
-    line = sys.stdin.readline()
-    if not line:
-        break
-    line = line.strip()
-    match = re.match(regex, line)
-    if not match:
-        continue
-    string = line.split()
-    size += int(string[-1])
-    lists.append(string[-2])
-    num += 1
-    signals = signal.signal(signal.SIGINT, handle)
-    if num == 10 or signals:
-        print("Filse size: {}".format(size))
-        codes = [200, 301, 400, 401, 403, 404, 405, 500]
-        for c in codes:
-            sortedList = sorted(lists)
-            counts = sortedList.count(str(c))
-            if counts:
-                print('{}: {}'.format(c, counts))
-            continue
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
